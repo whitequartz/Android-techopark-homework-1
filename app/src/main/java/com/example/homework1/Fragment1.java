63b62ecb2e23a6de2e12b722e11af5386504b48a
BuildTools <unconfigured@null.spigotmc.org>
2019-03-27 22:31:55 +0300

    Update Fragment1.java

--- app/src/main/java/com/example/homework1/Fragment1.java
+++ app/src/main/java/com/example/homework1/Fragment1.java
@@ -7,7 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
